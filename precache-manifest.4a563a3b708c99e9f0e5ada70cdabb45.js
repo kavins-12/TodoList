74705c74a4c96d40851819e0f65a7ab363d7451d
kavins-12 <kavins1012@gmail.com>
2020-08-01 14:55:16 +0530

    Updates

---
+++ precache-manifest.4a563a3b708c99e9f0e5ada70cdabb45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cf581b84a078f94e722d146f32aa99",
+    "url": "/kavins-12/TodoList/index.html"
+  },
+  {
+    "revision": "b6faf747556f81ba3bbf",
+    "url": "/kavins-12/TodoList/static/css/main.bdf9d97b.chunk.css"
+  },
+  {
+    "revision": "4042d1a95e656cf382aa",
+    "url": "/kavins-12/TodoList/static/js/2.264d8171.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kavins-12/TodoList/static/js/2.264d8171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6faf747556f81ba3bbf",
+    "url": "/kavins-12/TodoList/static/js/main.60d2d029.chunk.js"
+  },
+  {
+    "revision": "a6ca4195fcf9fe4a95ea",
+    "url": "/kavins-12/TodoList/static/js/runtime-main.d6e74c5c.js"
+  }
+]);
